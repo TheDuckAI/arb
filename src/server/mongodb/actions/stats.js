362bc95e0998dbf962d23d86cbb0d0a03aca68e3
Broyojo <dha@xoba.com>
2023-10-11 17:20:13 -0400

    added finding stats by id

--- src/server/mongodb/actions/stats.js
+++ src/server/mongodb/actions/stats.js
@@ -5,6 +5,13 @@ import {
     physicsSymbolicStatsModel
 } from "../models/stats_models.js";
 
+async function findOneByMongoId(model, id, projection) {
+    if (!ObjectId.isValid(id)) {
+        throw new Error("Invalid MongoDB ID format");
+    }
+    return await model.findById(id, projection);
+}
+
 async function getStatsForMathNumericalProblems() {
     return await mathNumericalStatsModel.find({}, {
         "Number of Questions": 1,
@@ -37,9 +44,46 @@ async function getStatsForPhysicsSymbolicProblems() {
     });
 }
 
+async function findStatsForMathNumericalProblemsById(id) {
+    return findOneByMongoId(mathNumericalStatsModel, id, {
+        "Number of Questions": 1,
+        "Percentage (%)": 1,
+        "Subject Name": 1,
+    });
+}
+
+async function findStatsForMathSymbolicProblemsById(id) {
+    return findOneByMongoId(mathSymbolicStatsModel, id, {
+        "Number of Questions": 1,
+        "Percentage (%)": 1,
+        "Subject Name": 1,
+    });
+}
+
+async function findStatsForPhysicsNumericalProblemsById(id) {
+    return findOneByMongoId(physicsNumericalStatsModel, id, {
+        "Number of Questions": 1,
+        "Percentage (%)": 1,
+        "Subject Name": 1,
+    });
+}
+
+async function findStatsForPhysicsSymbolicProblemsById(id) {
+    return findOneByMongoId(physicsSymbolicStatsModel, id, {
+        "Number of Questions": 1,
+        "Percentage (%)": 1,
+        "Subject Name": 1,
+    });
+}
+
 export {
+    findStatsForMathNumericalProblemsById,
+    findStatsForMathSymbolicProblemsById,
+    findStatsForPhysicsNumericalProblemsById,
+    findStatsForPhysicsSymbolicProblemsById,
     getStatsForMathNumericalProblems,
     getStatsForMathSymbolicProblems,
     getStatsForPhysicsNumericalProblems,
     getStatsForPhysicsSymbolicProblems
 };
+
